efb2892f72ffad920c488e90e1f4e5e008f8c7e1
nurasma <nurasma.shamsuddin@gmail.com>
2019-06-19 12:16:21 +0800

    change menu

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "<p><a href='movie_add.php'>Add Movie</a></p>";
-echo "<p><a href='movie_list.php'>Movie List</a></p>";
-echo "<p><a href='movie_search.php'>Search Movie</a></p>";
+echo "<p><a href='User_add.php'>Add Movie</a></p>";
+echo "<p><a href='User_list.php'>Movie List</a></p>";
+echo "<p><a href='User_search.php'>Search Movie</a></p>";
 
 ?>
